fe2ecf29cc01c293ef0daee977bab9745f9f2f19
jcoddamn <jcod10306@gmail.com>
2026-03-14 23:32:49 -0500

    Update faceViewer.js

--- faceViewer.js
+++ faceViewer.js
@@ -1,6 +1,6 @@
-import * as THREE from './js/three.module.js';
-import { GLTFLoader } from './js/GLTFLoader.js';
-import { OrbitControls } from './js/OrbitControls.js';
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js";
 
 let scene;
 let camera;
