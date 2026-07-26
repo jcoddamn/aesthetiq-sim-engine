514a28ff48b6ceff19a04986191e0a75d382d64b
jcoddamn <jcod10306@gmail.com>
2026-07-26 07:19:34 -0500

    Create lipProfiles.js

---
+++ js/lipProfiles.js
@@ -0,0 +1,27 @@
+const LIP_PROFILES = {
+
+  natural: {
+    cupidBow: 0.20,
+    upperVolume: 0.30,
+    lowerVolume: 0.35,
+    border: 0.15,
+    cornerLift: 0.03
+  },
+
+  balanced: {
+    cupidBow: 0.40,
+    upperVolume: 0.55,
+    lowerVolume: 0.60,
+    border: 0.30,
+    cornerLift: 0.05
+  },
+
+  enhanced: {
+    cupidBow: 0.65,
+    upperVolume: 0.90,
+    lowerVolume: 0.95,
+    border: 0.45,
+    cornerLift: 0.08
+  }
+
+};
