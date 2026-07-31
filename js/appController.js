14444832adf5abb10d1d91155c14a9f44737b6c7
jcoddamn <jcod10306@gmail.com>
2026-07-31 15:17:28 -0500

    Add runProcedureSimulationFromLandmarks import

--- js/appController.js
+++ js/appController.js
@@ -6,6 +6,7 @@ import {
 
 import {
   runProcedureSimulationFromImage,
+  runProcedureSimulationFromLandmarks,
   renderCanvasToElement,
   renderResultsToTargets
 } from "./simulationPipeline.js";
