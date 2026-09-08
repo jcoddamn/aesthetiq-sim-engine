bc393e7e1682a4879a1baefc48927b4fa657cb66
jcoddamn <jcod10306@gmail.com>
2026-09-08 16:49:59 -0500

    Update faceWarp.js

--- js/faceWarp.js
+++ js/faceWarp.js
@@ -1014,10 +1014,10 @@ const biomechanicalResult =
 
 const skinStrength =
   level === "natural"
-    ? 0.42
+    ? 0.2
     : level === "enhanced"
-    ? 0.18
-    : 0.3;
+    ? 0.48
+    : 0.33;
 
 return displaceLipSkin(
   landmarks,
