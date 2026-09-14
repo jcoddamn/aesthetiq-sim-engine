e957594f5fe7c25833dbca7f39719ee1763e16e8
jcoddamn <jcod10306@gmail.com>
2026-09-14 02:05:15 -0500

    Update appController.js

--- js/appController.js
+++ js/appController.js
@@ -9,7 +9,7 @@ import {
   runProcedureSimulationFromLandmarks,
   renderCanvasToElement,
   renderResultsToTargets
-} from "./simulationPipeline.js?v=2";
+} from "./simulationPipeline.js?v=3";
 
 import {
   getProcedureLabel,
